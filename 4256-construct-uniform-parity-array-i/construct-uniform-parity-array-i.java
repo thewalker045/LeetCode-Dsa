508a37e133c3f249754d128aecab92624ff7e995
thewalker045 <2024cs_diwakarsharmaaditya_a@nie.ac.in>
2026-09-02 23:34:11 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (5.31%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,23 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n=nums1.length;
-        int even=0;
-        int odd=0;
-        for(int i=0;i<n;i++){
-            if(nums1[i]%2==0){
-                even++;
-                
-            }
-            else{
-                odd++;
-            }
-        }
-        if(odd==0){
-            return true;
-        }
-        if(even==0){
-            return true;
-        }
-        return true;
+       return true;
     }
 }
