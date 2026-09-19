d71be8e877a5124302c6d5736be2bb7eef6aaaa2
thewalker045 <2024cs_diwakarsharmaaditya_a@nie.ac.in>
2026-09-19 15:52:25 +0530

    Time: 4 ms (17.39%) | Memory: 47.5 MB (92.59%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,13 +1,13 @@
 class Solution {
 
     int[][] dir = {
-            { -1, 0 },
-            { 0, -1 },
-            { 1, 0 },
-            { 0, 1 }
+        {-1, 0},
+        {0, -1},
+        {1, 0},
+        {0, 1}
     };
 
-    public List<int[]> regions(char[][] board, int x, int y, int[][] visited) {
+    public void bfs(char[][] board, int x, int y, int[][] visited) {
 
         int n = board.length;
         int m = board[0].length;
@@ -15,9 +15,11 @@ class Solution {
         Queue<int[]> q = new LinkedList<>();
         List<int[]> coordinates = new ArrayList<>();
 
-        q.offer(new int[] { x, y });
+        q.offer(new int[]{x, y});
         visited[x][y] = 1;
-        coordinates.add(new int[] { x, y });
+        coordinates.add(new int[]{x, y});
+
+        boolean boundary = false;
 
         while (!q.isEmpty()) {
 
@@ -26,46 +28,32 @@ class Solution {
             int i = curr[0];
             int j = curr[1];
 
+            if (i == 0 || i == n - 1 ||
+                j == 0 || j == m - 1) {
+                boundary = true;
+            }
+
             for (int[] d : dir) {
 
                 int ni = i + d[0];
                 int nj = j + d[1];
 
                 if (ni >= 0 && ni < n &&
-                        nj >= 0 && nj < m &&
-                        visited[ni][nj] == 0 &&
-                        board[ni][nj] == 'O') {
+                    nj >= 0 && nj < m &&
+                    visited[ni][nj] == 0 &&
+                    board[ni][nj] == 'O') {
 
                     visited[ni][nj] = 1;
-                    q.offer(new int[] { ni, nj });
-                    coordinates.add(new int[] { ni, nj });
+                    q.offer(new int[]{ni, nj});
+                    coordinates.add(new int[]{ni, nj});
                 }
             }
         }
 
-        return coordinates;
-    }
-
-    public boolean touchesBoundary(List<int[]> coordinates, int n, int m) {
-
-        for (int[] p : coordinates) {
-
-            int x = p[0];
-            int y = p[1];
-
-            if (x == 0 || x == n - 1 ||
-                    y == 0 || y == m - 1) {
-                return true;
+        if (!boundary) {
+            for (int[] p : coordinates) {
+                board[p[0]][p[1]] = 'X';
             }
-        }
-
-        return false;
-    }
-
-    public void convert(List<int[]> coordinates, char[][] board) {
-
-        for (int[] p : coordinates) {
-            board[p[0]][p[1]] = 'X';
         }
     }
 
@@ -80,12 +68,7 @@ class Solution {
             for (int j = 0; j < m; j++) {
 
                 if (board[i][j] == 'O' && visited[i][j] == 0) {
-
-                    List<int[]> region = regions(board, i, j, visited);
-
-                    if (!touchesBoundary(region, n, m)) {
-                        convert(region, board);
-                    }
+                    bfs(board, i, j, visited);
                 }
             }
         }
